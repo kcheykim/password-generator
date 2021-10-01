7d736f0f9ee7da842fa3506893e4c3b07e9e5439
kcheykim <kosalcheykim@gmail.com>
2021-09-30 20:59:59 -0700

    function pwdLength made sure the lenght is between 8-128

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-if (generateBtn) {
+var pwLength = 0;
+/*if (generateBtn) {
   var pwLength = window.prompt("Enter the length of your password (8 - 128): ");
   if(pwLength < 8 || pwLength > 128){
     window.prompt("Please hit the button again to enter a value between 8 - 128. Good-bye!");
   }
-}
+}*/
+
+
 // Write password to the #password input
 function writePassword() {
+
+  pwdLength();
+  window.prompt(pwLength);
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -23,5 +28,13 @@ function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
 
+function pwdLength() {
+  if (generateBtn) {
+    pwLength = window.prompt("Enter the length of your password (8 - 128): ");
+    while(pwLength < 8 || pwLength > 128){
+      pwLength = window.prompt("Please enter a length between 8 - 128 characters long: ");
+    }
+  }
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
