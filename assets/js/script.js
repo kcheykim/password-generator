adfd87bb0eea977196e67d48a6f20ae8c85659ea
kcheykim <kosalcheykim@gmail.com>
2021-09-30 20:30:17 -0700

    initial code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+if (generateBtn) {
+  var pwLength = window.prompt("Enter the length of your password (8 - 128): ");
+  if(pwLength < 8 || pwLength > 128){
+    window.prompt("Please hit the button again to enter a value between 8 - 128. Good-bye!");
+  }
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,6 +14,13 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+function getRandomNum() {
+  var x = Math.floor(Math.random()*10);
+}
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
 
 // Add event listener to generate button
