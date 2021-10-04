ef19413556d1a71a1d8c9f050e16e9003284fa73
kcheykim <kosalcheykim@gmail.com>
2021-10-03 22:24:08 -0700

    generating password and writing password to the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,121 @@
-// Assignment Code
+//declare global variables
 var generateBtn = document.querySelector("#generate");
-var length = null;
-var numeric = null;
-var special = null;
-var upper = null;
-var lower = null;
+var genPassWd = "";
 
-var randomChars = {
-  numChar: getRandomNum(),
-  specialChar: getRandomChars(),
-  upperChar: getRandomUpper(),
-  lowerChar: getRandomLower()
-}
 
 //this function gives the users choices of numeric, special character, uppercase or lowercase letters in their password
 function getChoices() {
+
+  //local variable for password length
+  var pwLength = 0;
+
+  //counter set increase as it check the users for the choices 
+  var counter = 0;
+  //empty string to concatenate the choices to one whole string
+  var randomString = "";
+
   //get user input on the length of pass that is between 8 to 128
   if (generateBtn) {
-    length = window.prompt("Enter the length of your password (8 - 128): ");
-    while(length < 8 || length > 128){
-      length = window.prompt("Please enter a length between 8 - 128 characters long: ");
+    var pwLength = window.prompt("Enter the length of your password (8 - 128): ");
+    while(pwLength < 8 || pwLength > 128 || isNaN(pwLength)) {
+        pwLength = window.prompt("Please re-enter a length between 8 - 128: ");
+      }
+  }
+  
+  while(counter == 0){
+
+    //check if user would like numeric values in password
+    //true: add random number to genPassWd by calling getRandomNum function,
+    //      add all numeric values to randomString, increase counter
+    var numeric = prompt("Do you want numeric value in your password: Enter '1' for Yes");
+    if(numeric == 1){
+      genPassWd += getRandomNum();
+      randomString += "0123456789";
+      counter++;
+    }
+
+    //check if the user would like special characters in password
+    //true: add random special character to genPassWd by calling getRandomChars function,
+    //      add all special character to randomString, increase counter
+    var special = prompt("Do you want special character in your password: Enter '1' for Yes");
+    if(special == 1){
+      genPassWd += getRandomChars();
+      randomString += "~!@#$%^&*()_+{}|:'<>?`-=[]\;,./";
+      counter++;
+    }
+
+    //check if the user would like uppercase letters in password
+    //true: add random uppercase letter to genPassWd by calling getRandomUpper function,
+    //      add all uppercase letters to randomString, increase counter
+    var upper = prompt("Do you want uppercase letter in your password: Enter '1' for Yes");
+    if(upper == 1){
+      genPassWd += getRandomUpper();
+      randomString += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      counter++;
+    }
+
+    //check if the user would like lowercase letters in password
+    //true: add random lowercase letter to genPassWd by calling getRandomLower function,
+    //      add all lowercase letters to randomString, increase counter
+    var lower = prompt("Do you want lowercase letter in your password: Enter '1' - Yes");
+    if(lower == 1){
+      genPassWd += getRandomLower();
+      randomString += "abcdefghijklmnopqrstuvwxyz";
+      counter++;
     }
   }
+  //reseting the lenght to length - counter (# of choice between 1-4)
+  pwLength = pwLength - counter;
 
-  //get user input on whether the password contains numeric values
-  numeric = prompt("Do you want numeric value in your password: Enter '0' - No and '1' - Yes");
-  while(!(numeric == 1 || numeric == 0)){
-    numeric = prompt("Please enter '1' for numeric values or '0' for no numeric value in your password. ");
-  }
-
-  //get user input on whether the passowrd contains special characters
-  special = prompt("Do you want special character in your password: Enter '0' - No and '1' - Yes");
-  while(!(special == 1 || special == 0)){
-    special = prompt("Please enter '1' for special character or '0' for no special character in your password. ");
-  }
-
-   //get user input on whether the passowrd contains uppercase letters
-   upper = prompt("Do you want uppercase letter in your password: Enter '0' - No and '1' - Yes");
-   while(!(upper == 1 || upper == 0)){
-     upper = prompt("Please enter '1' for uppercase letter or '0' for no uppercase letter in your password. ");
-   }
-
-  //get user input on whether the passowrd contains lowercase letters
-  lower = prompt("Do you want lowercase letter in your password: Enter '0' - No and '1' - Yes");
-  while(!(lower == 1 || lower == 0)){
-    lower = prompt("Please enter '1' for lowercase letter or '0' for no lowercase letter in your password. ");
-  }
-}
-  
-// Write password to the #password input
-function writePassword() {
-  getChoices();
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-//password = document.getElementByID('pass');
-
+  //calling generatePassword function - passing it the newlenght and the randomString
+  generatePassword(pwLength, randomString);
+ 
 }
 
+//function to generate random numeric value
+function generatePassword(pwdLen, rString) {
+  for(var i = 0; i < pwdLen; i++) {
+    genPassWd += rString[Math.floor(Math.random() * rString.length)]; 
+    console.log(genPassWd);
+  }
+  return genPassWd;
+}
+
+
+//function to generate random numeric value
 function getRandomNum() {
   return String.fromCharCode(Math.floor(Math.random()*10) +48);
 }
 
+//function to generate lowercase letter
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
+//function to generate uppercase letter
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
+//function to generate special character
 function getRandomChars() {
-  var a = String.fromCharCode(Math.floor(Math.random() * 16) + 32);
-  var b = String.fromCharCode(Math.floor(Math.random() * 7) + 58);
-  var c = String.fromCharCode(Math.floor(Math.random() * 6) + 91);
-  var d = String.fromCharCode(Math.floor(Math.random() * 4) + 123);
-  var allSpecial = a + b + c + d;
-  return allSpecial;
+  var sChars = '~!@#$%^&*()_+-={}|:"<>?[]\;,./';
+  return sChars[Math.floor(Math.random() * sChars.length)];
+}
 
+// Write password to the #password input
+function writePassword() {
+  
+ var password = getChoices();
+ password = generatePassword();
+ var passwordText = document.querySelector("#password");
+ passwordText.value = password;
+}
+
+function clearText()
+{
+  document.getElementById("#password").value='';
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
